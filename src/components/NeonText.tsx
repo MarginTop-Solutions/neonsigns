7afe79293c158aaf943d8e52b79645569f4aab3c
roshankc123 <kcroshan566@gmail.com>
2024-05-01 22:05:41 +0545

    bg image fix

--- src/components/NeonText.tsx
+++ src/components/NeonText.tsx
@@ -3,6 +3,7 @@
 import { IProps } from "@/utils/Interfaces"
 import { getNeonShadow } from "@/utils/misc";
 import { useState } from "react";
+import image1 from '../assets/Images/lounge1.jpg'
 
 export default function NeonText({ props }: { props: IProps }) {
     const [neonOn, setNeonOn] = useState(true);
@@ -11,6 +12,7 @@ export default function NeonText({ props }: { props: IProps }) {
 
     return (
         <div className="relative flex-grow flex flex-col items-center justify-center p-12 shadow-lg rounded-xl">
+            <img src={image1.src} className="w-full h-full absolute" style={{zIndex:-1}}/>
             <button
                 onClick={_ => setNeonOn(!neonOn)}
                 className="absolute top-2 left-2 rounded-md px-4 py-2 font-bold"
@@ -31,6 +33,7 @@ export default function NeonText({ props }: { props: IProps }) {
                         fontFamily: props.font,
                         fontSize: "5.5rem",
                         color: props.color
+                        
                     }}
             >
                 {props.text || "Your Text"}
